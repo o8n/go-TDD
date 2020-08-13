15474b8015c4a11b9266ee692cf420134c753e04
okamotchan <m.okamotchan@gmail.com>
2020-08-13 23:27:58 +0900

    Hello World with test

---
+++ hello.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+const englishHelloPrefix  = "Hello, "
+const spanish  = "Spanish"
+const spanishHelloPrefix  = "Hola, "
+const frenchHelloPrefix = "Bonjour、"
+const french  = "France"
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func main()  {
+	fmt.Println(Hello("world", ""))
+}
